75cc271fc02b52e2ba3e950eefe4d5e00b32d18c
chentao <18217613265@163.com>
2020-08-25 16:19:56 +0800

    修改

---
+++ src/main/java/com/ccty/noah/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.ccty.noah.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author 缄默
+ * @date   2020/08/25
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
